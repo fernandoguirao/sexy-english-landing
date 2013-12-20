56f786b32272f7ce7dc25409158aa8580ac7e3b0
Fernando Guirao <fernando@bueninvento.es>
2013-12-20 23:37:06 +0100

    Sexy

--- views/includes/meta.php
+++ views/includes/meta.php
@@ -1,4 +1,4 @@
 <meta charset="utf-8">
 <title>Sexy English</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" href="static/flat-ui/images/favicon.ico">
+<link rel="shortcut icon" href="static/img/favicon.ico">
--- views/sections/foot/index.php
+++ views/sections/foot/index.php
@@ -1,9 +1,8 @@
 <div id="bgVideo" class="background">
   <video id="video_background" preload="auto" autoplay="autoplay" loop="loop" width="100%">
-    <source src="media/sexy-english.mp4" type="video/mp4">
-    <source src="media/sexy-english.ogg" type="video/ogg">
-    <source src="media/sexy-english.webm" type="video/webm">
-    bgvideo
+    <source src="media/sexy.mp4" type="video/mp4">
+    <source src="media/sexy.ogg" type="video/ogg">
+    <source src="media/sexy.webm" type="video/webm">
   </video>
 <!--
   <div class="grad-back">
